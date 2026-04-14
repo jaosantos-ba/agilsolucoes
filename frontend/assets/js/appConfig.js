eccfb1fb1ae56469af88b15e14a719bdb87115fd
José Antonio Santos <joseantonio.santos@ucsal.edu.br>
2026-04-14 18:19:05 -0300

    Update page 20260414_181900

--- frontend/assets/js/appConfig.js
+++ frontend/assets/js/appConfig.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  whatsappNumber: "5571981967353",
+  whatsappNumber: "5571920043756",
   appsScriptUrl: "https://script.google.com/macros/s/AKfycby5FRBhWo_8XZebZ7HekYlkB1_OT8Ihdv_s6cGGsaNCnnQ1UG0sCmVh90Sez5jKzzxj/exec",
   redirectToWhatsApp: true
 };
